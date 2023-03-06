b05e40f6b3c53f8aa271910f5e89ba042c080451
Jonathan Tsai <modopo@gmail.com>
2023-03-06 14:45:08 -0800

    search returns api data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SearchForm from './SearchForm';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchInput: '',
+      display_name: '',
+      latitude: 0,
+      longitude: 0,
+      errorMessage: '',
+    }
+  }
+
+  handleSearchInput = (e) => {
+    this.setState({
+      searchInput: e.target.value
+    });
+  }
+
+  searchSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      let cityData = await axios.get(`https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.searchInput}&format=json`);
+
+      this.setState({
+        display_name: cityData.data[0].display_name,
+        latitude: cityData.data[0].lat,
+        longitude: cityData.data[0].lon
+      });
+    } catch (error) {
+      console.log('error: ', error);
+      console.log('error.message: ', error.message);
+      this.setState({
+        errorMessage: error.message,
+      });
+    }
+  }
+
+  render() {
+    return (
+      <SearchForm
+        searchSubmit={this.searchSubmit}
+        searchInput={this.handleSearchInput}
+      />
+    );
+  }
 }
 
 export default App;
---
+++ src/SearchForm.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+
+class SearchForm extends React.Component {
+  
+  render() {
+    
+    return (
+      <Form onSubmit={this.props.searchSubmit}>
+        <Form.Control 
+          type="text" 
+          placeholder="City Name"
+          onChange={this.props.searchInput}
+        />
+        <Button type="submit">Explore!</Button>
+      </Form>
+    );
+  }
+  
+}
+
+export default SearchForm;
