93d2dc50e981d02f92c0c6fa2c466b9af521351d
Jonathan Tsai <modopo@gmail.com>
2023-03-06 15:25:00 -0800

    data in Card component in City

--- src/City.js
+++ src/City.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 class City extends React.Component{
 
   render() {
     return (
-      <>
-        <h2>{this.props.data.display_name}</h2>
-        <p>Latitude: {this.props.data.latitude}</p>
-        <p>Longitude: {this.props.data.longitude}</p>
-      </>
+      <Card>
+        <Card.Header as="h2">{this.props.data.display_name}</Card.Header>
+        <Card.Body>
+          Latitude: {this.props.data.latitude}
+        </Card.Body>
+        <Card.Body>
+          Longitude: {this.props.data.longitude}
+        </Card.Body>
+      </Card>
     );
   }
-
-
 }
 
 export default City;
