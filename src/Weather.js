013c58eb985bafbc5edb604fcbadefc582a09af5
Jonathan Tsai <modopo@gmail.com>
2023-03-10 14:42:12 -0800

    removed console log

--- src/Weather.js
+++ src/Weather.js
@@ -5,7 +5,6 @@ import './css/Weather.css';
 class Weather extends React.Component {
 
   render () {
-    console.log(this.props.forecasts);
     let daysForecast = this.props.forecasts.map((day) => {
       return (
         <Card.Text key={day.date}>{day.date}: Low of {day.low} °C, high of {day.high} C° with {day.forecast.toLowerCase()}</Card.Text>
